67fe6e8a23e52fa92460fc684dcaa8ca9e24a687
Bharat Mane <bharat.mane@gmail.com>
2021-09-29 18:33:18 +0530

    feat: demo for Daemon

--- ThreadPriorityDemo/src/io/github/bharatmane/Main.java
+++ ThreadPriorityDemo/src/io/github/bharatmane/Main.java
@@ -3,15 +3,15 @@ package io.github.bharatmane;
 public class Main {
 
     public static void main(String[] args) {
-	    Thread thread1 = new Thread(new SampleThread());
-        Thread thread2 = new Thread(new SampleThread());
-        Thread thread3 = new Thread(new SampleThread());
+	    Thread thread1 = new Thread(new SampleThread("Thread 1"));
+        Thread thread2 = new Thread(new SampleThread("Thread 2"));
+        Thread thread3 = new Thread(new SampleThread("Thread 3"));
 
         System.out.println("Thread1 Priority : " + thread1.getPriority());
         System.out.println("Thread2 Priority : " + thread2.getPriority());
         System.out.println("Thread3 Priority : " + thread3.getPriority());
 
-        thread1.setPriority(1);
+        thread1.setPriority(9);
         thread2.setPriority(4);
         thread3.setPriority(9);
 
@@ -19,9 +19,14 @@ public class Main {
         System.out.println("New Thread2 Priority : " + thread2.getPriority());
         System.out.println("New Thread3 Priority : " + thread3.getPriority());
 
+        thread1.setDaemon(true);
+
         thread1.start();
         thread2.start();
         thread3.start();
+
+
+        System.out.println("New Thread1 Priority : " + thread1.getPriority());
 
     }
 }
--- ThreadPriorityDemo/src/io/github/bharatmane/SampleThread.java
+++ ThreadPriorityDemo/src/io/github/bharatmane/SampleThread.java
@@ -2,8 +2,15 @@ package io.github.bharatmane;
 
 public class SampleThread implements Runnable{
 
+    String name;
+
+    public SampleThread(String name)
+    {
+        this.name = name;
+    }
+
     @Override
     public void run() {
-        System.out.println("Inside run method");
+        System.out.println("Inside run method Thread Name: " + name);
     }
 }
