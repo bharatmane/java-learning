40991984bf6cd620acc559112bbce70c166f486e
Bharat Mane <bharat.mane@gmail.com>
2021-09-29 18:26:45 +0530

    feat: basic demo of thread priority

---
+++ ThreadPriorityDemo/src/io/github/bharatmane/Main.java
@@ -0,0 +1,28 @@
+package io.github.bharatmane;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Thread thread1 = new Thread(new SampleThread());
+        Thread thread2 = new Thread(new SampleThread());
+        Thread thread3 = new Thread(new SampleThread());
+
+        System.out.println("Thread1 Priority : " + thread1.getPriority());
+        System.out.println("Thread2 Priority : " + thread2.getPriority());
+        System.out.println("Thread3 Priority : " + thread3.getPriority());
+
+        thread1.setPriority(1);
+        thread2.setPriority(4);
+        thread3.setPriority(9);
+
+        System.out.println("New Thread1 Priority : " + thread1.getPriority());
+        System.out.println("New Thread2 Priority : " + thread2.getPriority());
+        System.out.println("New Thread3 Priority : " + thread3.getPriority());
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+    }
+}
+
---
+++ ThreadPriorityDemo/src/io/github/bharatmane/SampleThread.java
@@ -0,0 +1,9 @@
+package io.github.bharatmane;
+
+public class SampleThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Inside run method");
+    }
+}
