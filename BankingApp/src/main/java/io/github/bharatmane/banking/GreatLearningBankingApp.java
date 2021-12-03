050695f2baad339a82409315f51256268243026f
bharat.mane <bharat.mane@outlook.com>
2021-12-03 11:28:19 +0530

    feat:initial proejct setup

---
+++ BankingApp/src/main/java/io/github/bharatmane/banking/GreatLearningBankingApp.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatLearningBankingApp {
+    public static void main(String[] args) {
+
+    }
+}
