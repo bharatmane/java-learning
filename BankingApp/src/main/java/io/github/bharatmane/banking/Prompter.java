6ca1393b374bd41d154af752123ec2fd36f09622
bharat.mane <bharat.mane@outlook.com>
2021-12-04 16:41:16 +0530

    feat: added basic options for login

--- BankingApp/src/main/java/io/github/bharatmane/banking/Prompter.java
+++ BankingApp/src/main/java/io/github/bharatmane/banking/Prompter.java
@@ -1,4 +1,34 @@
 package io.github.bharatmane.banking;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
+
 public class Prompter {
+    private final PrintStream stdOut;
+    private Scanner scanner = new Scanner(System.in);
+    private String option;
+    public Prompter(PrintStream stdOut) {
+        this.stdOut = stdOut;
+    }
+
+    public void greetUser() {
+        print("Welcome to Great Learning Banking Services!");
+    }
+
+    private void print(String message) {
+        stdOut.println(message);
+    }
+
+    private void prompt(String message) {
+        stdOut.println("> ");
+        option = scanner.nextLine();
+
+    }
+
+    public void promptLogin() {
+        print("Choose Option");
+        print("1. Login");
+        print("2. Exit");
+    }
 }
--- BankingApp/src/test/java/io/github/bharatmane/banking/PrompterTest.java
+++ BankingApp/src/test/java/io/github/bharatmane/banking/PrompterTest.java
@@ -1,13 +1,51 @@
 package io.github.bharatmane.banking;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = Prompter.class)
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+
 public class PrompterTest {
+
+    @Test
+    @DisplayName("should prompt welcome message")
+    void shouldPromptWelcomeMessage(){
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        String expectedGreetMessage = "Welcome to Great Learning Banking Services!";
+        //Given
+        Prompter prompter = new Prompter(printStream);
+        //When
+        prompter.greetUser();
+
+        //Then
+        assertThat(outputStream.toString()).contains(expectedGreetMessage);
+    }
+
+    @Test
+    @DisplayName("should prompt login options")
+    void shouldPromptLoginOptions(){
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        String expectedLoginString = "1. Login";
+        String expectedExitString = "1. Login";
+        //Given
+        Prompter prompter = new Prompter(printStream);
+        //When
+        prompter.promptLogin();
+
+        //Then
+        assertThat(outputStream.toString()).contains(expectedLoginString);
+        assertThat(outputStream.toString()).contains(expectedExitString);
+    }
 }
